58ff2ef5e69e8f6e10794875d7f781adda2d87bd
Tisha Dey <tishadey2025@outlook.com>
2026-02-08 01:17:41 +0600

    :rocket: problem three solved

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ function newPrice(currentPrice, discount) {
 // let output = newPrice(2000,17.17);
 // console.log(output);
 
-
 // problem-2
 
-function validOtp(otp){
-  if(otp.startsWith("ph")&& (otp.length===8)){
-   return true;
-  }else{
-   return false;
+function validOtp(otp) {
+  if (otp.startsWith("ph") && otp.length === 8) {
+    return true;
+  } else {
+    return false;
   }
 }
-// let output = validOtp("ph-123456746753") 
+// let output = validOtp("ph-123456746753")
+// console.log(output);
+
+// problem-3
+
+function finalScore(omr) {
+  let value = omr.right + omr.wrong + omr.skip;
+  if (value == 100) {
+    let num = omr.right * 1 - omr.wrong * 0.5 + omr.skip * 0;
+    let result = Math.round(num);
+    return result;
+  } else {
+    return "invalid";
+  }
+}
+
+// let output = finalScore(["Raj"]);
 // console.log(output);
