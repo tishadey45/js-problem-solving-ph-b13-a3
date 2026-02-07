e69130ed620ccbcb97f3a3c75eb8e94dfa81307a
Tisha Dey <tishadey2025@outlook.com>
2026-02-07 14:59:04 +0600

    :fire: js problem done

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function newPrice(currentPrice, discount) {
 }
 // let output = newPrice(2000,17.17);
 // console.log(output);
+
+
+// problem-2
+
+function validOtp(otp){
+  if(otp.startsWith("ph")&& (otp.length===8)){
+   return true;
+  }else{
+   return false;
+  }
+}
+// let output = validOtp("ph-123456746753")
+// console.log(output);
